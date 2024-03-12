e063ddafe4b83fe1e5493ec55430711989539aef
JAIRO-99 <jairo.lcelis1999@gmail.com>
2024-03-11 19:04:17 -0500

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by New on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
